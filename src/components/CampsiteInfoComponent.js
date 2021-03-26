891226b73d9bb70d9c1c2b37db7edcb620bceb14
Delancey Vela <delancey.vela@gmail.com>
2021-03-25 23:07:03 -0400

    fetch post comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -41,7 +41,7 @@ class CommentForm extends Component {
   };
 
   handleSubmit = (values) => {
-    this.props.addComment(
+    this.props.postComment(
       this.props.campsiteId,
       values.rating,
       values.author,
@@ -143,7 +143,7 @@ function RenderCampsite({ campsite }) {
   );
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -163,7 +163,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
             </div>
           );
         })}
-        <CommentForm campsiteId={campsiteId} addComment={addComment} />
+        <CommentForm campsiteId={campsiteId} postComment={postComment} />
       </div>
     );
   }
@@ -208,7 +208,7 @@ function CampsiteInfo(props) {
           <RenderCampsite campsite={props.campsite} />
           <RenderComments
             comments={props.comments}
-            addComment={props.addComment}
+            postComment={props.postComment}
             campsiteId={props.campsite.id}
           />
         </div>
--- src/redux/comments.js
+++ src/redux/comments.js
@@ -8,8 +8,6 @@ export const Comments = (state = { errMess: null, comments: [] }, action) => {
       return { ...state, errMess: action.payload };
     case ActionTypes.ADD_COMMENT:
       const comment = action.payload;
-      comment.id = state.comments.length;
-      comment.date = new Date().toISOString();
       return { ...state, comments: state.comments.concat(comment) }; // don't use push. concat will create a new but push would mutate the original
     default:
       return state;
