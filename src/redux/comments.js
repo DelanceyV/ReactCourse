b2012a213c3c470a9b9a69e19aa35eb32fbffbb5
Delancey Vela <delancey.vela@gmail.com>
2021-03-25 22:54:07 -0400

    fetch errors commit

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,13 +1,16 @@
-import { COMMENTS } from "../shared/comments";
 import * as ActionTypes from "./ActionTypes";
 
-export const Comments = (state = COMMENTS, action) => {
+export const Comments = (state = { errMess: null, comments: [] }, action) => {
   switch (action.type) {
+    case ActionTypes.ADD_COMMENTS:
+      return { ...state, errMess: null, comments: action.payload };
+    case ActionTypes.COMMENTS_FAILED:
+      return { ...state, errMess: action.payload };
     case ActionTypes.ADD_COMMENT:
       const comment = action.payload;
-      comment.id = state.length;
+      comment.id = state.comments.length;
       comment.date = new Date().toISOString();
-      return state.concat(comment); // don't use push. concat will create a new but push would mutate the original
+      return { ...state, comments: state.comments.concat(comment) }; // don't use push. concat will create a new but push would mutate the original
     default:
       return state;
   }
